ae10b8325b0f77aebe90448e5ffdfb2ab7a50987
segmentationfaulter <muhammadsackib@gmail.com>
2018-12-24 07:48:39 +0500

    remove style loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,10 +16,6 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
       }
     ]
   },
